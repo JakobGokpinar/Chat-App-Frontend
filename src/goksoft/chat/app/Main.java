63d56aefa6d99e90459d0e0a9362532559d3fb17
unknown <ahmettabar2003@gmail.com>
2021-03-06 18:48:57 +0100

    fs

--- src/goksoft/chat/app/Main.java
+++ src/goksoft/chat/app/Main.java
@@ -16,6 +16,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        System.out.println("Chat app on active");
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("userinterfaces/login.fxml"));
         Parent root = loader.load();
